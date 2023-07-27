55d42c64437c79e8bc9cfc3bc4a4ea7e848ffe56
takku3250 <102309421+takku3250@users.noreply.github.com>
2023-07-27 17:39:21 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,3 @@ dev_basis04
 Route::get("/posts/create",[PostController::class,"create"]);
 Route::get("/posts/{post}",[PostController::class,"show"]);
 Route::post('/posts', [PostController::class, 'store']);
-
-Route::get('/posts/{post}', [PostController::class ,'show']);
- master
